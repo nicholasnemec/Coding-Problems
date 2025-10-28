12665b8288cae84087a6828a110d58dcc2341d08
Nicholas Ivan Nemec <nicholasivannemec@gmail.com>
2025-10-28 18:11:06 -0400

    Create twoSum.c
    completed to TwoSum problem on LeetCode in python found at this link: https://leetcode.com/problems/two-sum

---
+++ LeetCode_Problems/twoSum.c
@@ -0,0 +1,23 @@
+/**
+1. Two Sum (EASY)
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+**/
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+ *returnSize = 2;
+ int* result = malloc(2 * sizeof(int));
+ 
+ for(int i = 0; i < numsSize; i++){
+	for(int j = i + 1; j<numsSize;j++){
+		if(nums[i] + nums[j] == target){
+			result[0] = i;
+			result[1] = j;
+			return result;
+		}
+	}
+  }
+  *returnSize = 0;
+  return NULL;
+}
